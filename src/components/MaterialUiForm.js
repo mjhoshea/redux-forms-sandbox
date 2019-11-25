af193c12c564b7201d9bb8f3bc765ccdd25b8b38
michael.oshea <michael.oshea@babylonhealth.com>
2019-11-25 11:33:57 +0000

    app changes

--- src/components/MaterialUiForm.js
+++ src/components/MaterialUiForm.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React, {Component} from "react";
 import {Field, reduxForm} from "redux-form";
-import TextField from '@material-ui/core/TextField'
 import {connect} from "react-redux";
-
-
+import TextField from "@material-ui/core/TextField";
 
 const renderTextField = ({
-    label,
-    input,
-    meta: { touched, invalid, error },
-    ...custom
-}) => (
+                             label,
+                             input,
+                             meta: { touched, invalid, error },
+                             ...custom
+                         }) => (
     <TextField
         label={label}
         placeholder={label}
@@ -21,21 +19,60 @@ const renderTextField = ({
     />
 )
 
-const MaterialUiForm = props => {
-    const { handleSubmit } = props;
-    return (
-        <form onSubmit={handleSubmit}>
+class MaterialUiForm extends Component {
+
+
+
+    render() {
+        console.log(this.props)
+        return (
             <div>
-                 <Field
-                     name="firstName"
-                     componet={renderTextField}
-                     lable={'First Name'}
+            <h2>World</h2>
+            <form>
+                <div>
+                    <Field
+                        name="firstName"
+                        component={renderTextField}
+                        label="Project Name"
                     />
+                </div>
+                <div>
+                    <Field
+                        name="firstName"
+                        component={renderTextField}
+                        label="Project Goals"
+                    />
+                </div>
+                <div>
+                    <Field
+                        name="firstName"
+                        component={renderTextField}
+                        label="Project links"
+                    />
+                </div>
+                <div>
+                    <button type="submit" disabled={pristine || submitting}>
+                        Submit
+                    </button>
+                    <button type="button" disabled={pristine || submitting} onClick={reset}>
+                        Clear Values
+                    </button>
+                </div>
+            </form>
             </div>
-        </form>
-    )
+        )
+    }
+
 }
 
+const mapStateToProps = (state) => ({
+    a: state
+});
+MaterialUiForm = connect(mapStateToProps)(MaterialUiForm)
+
 export default reduxForm({
-    form: 'MaterialUiForm',
+    form: 'MaterialUiForm'
 })(MaterialUiForm)
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {reducer as formReducer} from 'redux-form'
+import createSagaMiddleware from 'redux-saga'
+import {Provider} from "react-redux";
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+        form: formReducer
+    }),
+    applyMiddleware(sagaMiddleWare)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
